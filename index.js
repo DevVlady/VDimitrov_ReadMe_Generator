6828d3601ea21cb045b35ebf4663b97458fd9e26
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-11-23 14:58:38 -0600

    Added the license badge in my red me file.

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ const questions = () => {
 function generateMarkdown (answer) {
     let READMEString =
 `# ${answer.projectName}
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 ## Description
 
